0c5bad808891908ec4b1e23adf386c3296aa3cbb
monali <monali.shinde@cuelogic.com>
2019-07-31 18:23:27 +0530

    added method wheelcount=(value) in SUPERCLASS

--- assignment2.rb
+++ assignment2.rb
@@ -4,20 +4,20 @@
 # wheel_count variable is overridden in case of Auto and Bus class, since they have 3 and 4 wheels respectively
 
 class Vehicle
-  def initialize wheel_count
+  def initialize
     @wheel_count = 2
   end
   def wheel_count
     "wheel count is #{@wheel_count}"
+  end
+  def wheel_count=(value)
+    @wheel_count = value
   end
 end
 
 class Bike < Vehicle
   def show
     "Bike have #{@wheel_count} wheels"
-  end
-    def wheel_count=(value)
-    @wheel_count = value
   end
 end
 
@@ -39,7 +39,7 @@ class Bus < Vehicle
   end
 end
 
-bike = Bike.new 10
+bike = Bike.new
 bike.show
 bike.wheel_count
 
