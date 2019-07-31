0828911472abd4c54723fdc10d2bfe93083b34ce
monali <monali.shinde@cuelogic.com>
2019-07-31 18:18:53 +0530

    added method wheelcount=(value)

--- assignment2.rb
+++ assignment2.rb
@@ -4,17 +4,20 @@
 # wheel_count variable is overridden in case of Auto and Bus class, since they have 3 and 4 wheels respectively
 
 class Vehicle
-  def initialize
+  def initialize wheel_count
     @wheel_count = 2
   end
   def wheel_count
-    puts "wheel count is #{@wheel_count}"
+    "wheel count is #{@wheel_count}"
   end
 end
 
 class Bike < Vehicle
   def show
-    puts "Bike have #{@wheel_count} wheels"
+    "Bike have #{@wheel_count} wheels"
+  end
+    def wheel_count=(value)
+    @wheel_count = value
   end
 end
 
@@ -23,7 +26,7 @@ class Auto < Vehicle
     @wheel_count = 3
   end
   def show
-    puts "Auto have #{@wheel_count} wheels"
+    "Auto have #{@wheel_count} wheels"
   end
 end
 
@@ -32,12 +35,15 @@ class Bus < Vehicle
     @wheel_count = 4
   end
   def show
-    puts "Bus have #{@wheel_count} wheels"
+    "Bus have #{@wheel_count} wheels"
   end
 end
 
-bike = Bike.new
+bike = Bike.new 10
 bike.show
+bike.wheel_count
+
+bike.wheel_count = 10
 bike.wheel_count
 
 auto = Auto.new
