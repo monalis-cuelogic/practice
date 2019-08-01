7ecee7ff98c2c44f99cc1cf1168fbcb352de2dd7
monali <monali.shinde@cuelogic.com>
2019-08-01 16:20:38 +0530

    done with assignment4 for fibbonacci series

---
+++ assignment4.rb
@@ -0,0 +1,22 @@
+# Assignment 4
+# Accept the number from user and for that many times, calculate the Fibonacci series and print the whole output. Eg: 
+# If user enters 4 then output should be 1,1,2,3
+# If user enters 7 then output should be 1,1,2,3,5,8,13
+
+class Fibonacci
+def fibo
+  puts "enter number"
+  number = gets.to_i
+  first = 0
+  second = 1
+  third = 0
+  number.times do
+    third = first + second
+    first = second
+    second = third
+  end
+end
+end
+
+ob = Fibonacci.new
+ob.fibo
