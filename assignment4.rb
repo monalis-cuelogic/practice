4220be922800f90eacae60b5075668e3774539e8
monali <monali.shinde@cuelogic.com>
2019-08-01 17:45:46 +0530

    removed third variable

--- assignment4.rb
+++ assignment4.rb
@@ -4,19 +4,20 @@
 # If user enters 7 then output should be 1,1,2,3,5,8,13
 
 class Fibonacci
-def fibo
+ def fibo
   puts "enter number"
-  number = gets.to_i
-  first = 0
-  second = 1
-  third = 0
-  number.times do
+   number = gets.to_i
+   first = 0
+   second = 1
+    number.times do
     third = first + second
+    puts third
     first = second
     second = third
-  end
-end
+    end
+ end
 end
 
 ob = Fibonacci.new
 ob.fibo
+
