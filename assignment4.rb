d6317d8eea9c436673dffd00615e6f7b2f316b34
monali <monali.shinde@cuelogic.com>
2019-08-02 20:08:45 +0530

    added print

--- assignment4.rb
+++ assignment4.rb
@@ -1,5 +1,5 @@
 # Assignment 4
-# Accept the number from user and for that many times, calculate the Fibonacci series and print the whole output. Eg: 
+# Accept the number from user and for that many times, calculate the Fibonacci series and print the whole output. Eg:
 # If user enters 4 then output should be 1,1,2,3
 # If user enters 7 then output should be 1,1,2,3,5,8,13
 
@@ -18,7 +18,8 @@ class Fibonacci
     n1 = n2
     n2 = n3
     n3 = n2 + n1
-    puts seq[sequence]
+    demo = seq[sequence]
+    print demo.to_s
     end
  end
 end
